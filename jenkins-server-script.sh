76767a795e821e53bfb16d2baacb8b80d19752db
YoungMinds2024 <ygminds2024@gmail.com>
2026-07-19 08:53:14 +0530

    Migrate jenkins-server-script to use apt package manager
    Updated the script to use apt for package management instead of yum, added system update commands, and modified installation steps for Jenkins, Git, and Terraform.

--- jenkins-server-script.sh
+++ jenkins-server-script.sh
@@ -1,29 +1,32 @@
 #!/bin/bash
 
+# update system
+sudo apt update -y
+sudo apt upgrade -y
+
 # install jenkins
-sudo yum update –y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
-sudo yum install jenkins -y
+sudo apt install -y fontconfig openjdk-17-jre
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install -y jenkins
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 
 # install git
-sudo yum install git -y
+sudo apt install -y git
 
 # install terraform
-
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
-
-# install kubectl
-
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+sudo apt install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+    sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install -y terraform
 
