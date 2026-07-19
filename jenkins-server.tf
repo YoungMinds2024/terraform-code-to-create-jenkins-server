91e584385c41f4adc18b0a0c82598359509c181c
YoungMinds2024 <ygminds2024@gmail.com>
2026-07-19 08:52:40 +0530

    Change AMI from Amazon Linux to Ubuntu

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -1,18 +1,19 @@
-data "aws_ami" "latest-amazon-linux-image" {
+data "aws_ami" "latest-ubuntu-image" {
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"] # Canonical's AWS account ID
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
 }
-
 resource "aws_instance" "myapp-server" {
-  ami                         = data.aws_ami.latest-amazon-linux-image.id
+  ami                         = data.aws_ami.latest-ubuntu-image.id
   instance_type               = var.instance_type
   key_name                    = "jenkins-server"
   subnet_id                   = aws_subnet.myapp-subnet-1.id
